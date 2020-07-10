3f19684d6109131f16e75c479fe727708804ec26
bhanu4675 <42282589+bhanu4675@users.noreply.github.com>
2020-07-10 23:32:48 +0530

    Update Calculator.java

--- src/com/mypackage/Calculator.java
+++ src/com/mypackage/Calculator.java
@@ -5,14 +5,32 @@ import java.util.stream.Stream;
 
 public class Calculator {
 	
+	private String delimiter;
+	private String numbers;
+	
+	private Calculator(String delimiter, String numbers) {
+		this.delimiter = delimiter;
+		this.numbers = numbers;
+	}
+	
 	public static int add(String input) {
 		if(input.isEmpty()) 
 			return 0;
 		
-		Stream<String> numbers = Arrays.stream(input.split(",|\n"));
+		Calculator cal;
+		if(input.startsWith("//")) {
+			String[] parts = input.split("\n",2);
+			input = parts[1];
+			cal = new Calculator(parts[0].substring(2),input);
+		}else{
+			cal = new Calculator(",|\n", input);
+		}
+		
+		Stream<String> numbers = Arrays.stream(cal.numbers.split(cal.delimiter));
 		return numbers.mapToInt(Integer::parseInt).sum();
 		}
 	
 
 }
+
 
